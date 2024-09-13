b9e6c5820ba537e62b03e2776f2c496e6f2e63f7
여운 <yeoun0570@gmail.com>
2024-09-13 10:11:29 +0900

    @Valid를 이용한 검증 추가

--- src/main/java/com/ssg/springtodoservice/dto/TodoDTO.java
+++ src/main/java/com/ssg/springtodoservice/dto/TodoDTO.java
@@ -2,6 +2,8 @@ package com.ssg.springtodoservice.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Data
@@ -9,11 +11,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
+// 스프링VMC에서는 파라미터 검증 작업은 컨트롤러에서진행한다.
+// @Valid 와 BindingResult 객체를 이용하여 처리한다.
+// hibernate-validate 라이브러리가 필요하다.
 public class TodoDTO {
     private int tno;
+
+    @NotEmpty
     private String title;
+
+    @Future
     private LocalDate dueDate;
+
+    @NotEmpty
     private String writer;
+
     private boolean finished;
 
 }
